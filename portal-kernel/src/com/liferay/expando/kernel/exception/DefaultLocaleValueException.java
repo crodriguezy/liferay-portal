4046748ef5263dfe4103eb57f82ea6ebc227ebb3
crodriguezy <cristina.rodriguez@liferay.com>
2022-07-19 11:15:01 +0200

    LPS-142380 Add validation in ExpandoColumnLocalServiceImpl

---
+++ portal-kernel/src/com/liferay/expando/kernel/exception/DefaultLocaleValueException.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.expando.kernel.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.Locale;
+
+/**
+ * @author Cristina Rodríguez
+ */
+public class DefaultLocaleValueException extends PortalException {
+
+	public DefaultLocaleValueException() {
+	}
+
+	public DefaultLocaleValueException(Locale locale) {
+		super(
+			"A value for the default locale (" + locale.getLanguage() +
+				") must be defined");
+	}
+
+	public DefaultLocaleValueException(String msg) {
+		super(msg);
+	}
+
+	public DefaultLocaleValueException(String msg, Throwable throwable) {
+		super(msg, throwable);
+	}
+
+	public DefaultLocaleValueException(Throwable throwable) {
+		super(throwable);
+	}
+
+}
